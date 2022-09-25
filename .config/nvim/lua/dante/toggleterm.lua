cb5e33bc3863450e7968008d0eb5951aa96726fb
dantearaujo1 <danteeng@hotmail.com>
2022-09-24 22:05:05 -0300

    Creating lazygit dotfiles function to work inside Windows and Linux

--- .config/nvim/lua/dante/toggleterm.lua
+++ .config/nvim/lua/dante/toggleterm.lua
@@ -1,4 +1,6 @@
 local Terminal = require("toggleterm.terminal").Terminal
+local util = require("utils")
+local dotfiles = {}
 local lazygit = Terminal:new(
   {
     dir = "git_dir",
@@ -10,17 +12,35 @@ local lazygit = Terminal:new(
     hidden=true
   }
 )
-local dotfiles = Terminal:new(
-{
-  dir = "git_dir",
-  cmd = "lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME",
-  direction = "float",
-  float_opts ={
-    border = "rounded"
-  },
-  hidden=true
-}
-)
+
+if (util.getOS() == 'Linux') then
+
+  dotfiles = Terminal:new(
+    {
+      dir = "git_dir",
+      cmd = "lazygit --git-dir=$HOME/.dotfiles --work-tree=$HOME",
+      direction = "float",
+      float_opts ={
+        border = "rounded"
+      },
+      hidden=true
+    }
+  )
+else
+  dotfiles = Terminal:new(
+    {
+      dir = "git_dir",
+      cmd = "lazygit --git-dir=%HOME%/.dotfiles --work-tree=%HOME%",
+      direction = "float",
+      float_opts ={
+        border = "rounded"
+      },
+      hidden=true
+    }
+  )
+
+
+end
 
 function _lazygit_toggle()
   lazygit:toggle()
