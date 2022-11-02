f9cdac27fd276b39edf5c1385f452e64038ebe8f
Dante Araújo <danteeng@hotmail.com>
2022-11-02 01:56:14 -0300

    Disabling default mappings inside our processing plugin

--- .config/nvim/lua/dante/processing.lua
+++ .config/nvim/lua/dante/processing.lua
@@ -1,6 +1,6 @@
 local g = vim.g -- Acess Global Variables
 
 g.processing_fold=1
-
+g.processing_no_default_mappings = 1
 g.processing_doc_style='web'
 -- g.processing_doc_path="D:/Code/Languages/Processing/processing-docs/contents/api_en"
