cfc8175aa67a8c04e114f07b7b7cc6840fc08fb9
Dante Araújo <danteeng@hotmail.com>
2022-12-14 14:26:14 -0300

    Correcting for using inside vscode too

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,26 +1,32 @@
 lua require 'init'
 
-if has('unix')
-	let g:tpipeline_cursormoved = 1
-  let &t_fe = "\<Esc>[?1004h"
-  let &t_fd = "\<Esc>[?1004l"
+if exists('g:vscode')
 
-	function s:AddTerminalNavigation()
+else
 
-	    echom &filetype
-	    if &filetype ==# ''
-		tnoremap <buffer> <silent> <c-h> <c-\><c-n>:TmuxNavigateLeft<cr>
-		tnoremap <buffer> <silent> <c-j> <c-\><c-n>:TmuxNavigateDown<cr>
-		tnoremap <buffer> <silent> <c-k> <c-\><c-n>:TmuxNavigateUp<cr>
-		tnoremap <buffer> <silent> <c-l> <c-\><c-n>:TmuxNavigateRight<cr>
-		tnoremap <Esc> <C-\><C-n>
-		tnoremap <C-v><Esc> <Esc>
-	    endif
+  if has('unix')
+    let g:tpipeline_cursormoved = 1
+    let &t_fe = "\<Esc>[?1004h"
+    let &t_fd = "\<Esc>[?1004l"
 
-	endfunction
+    function s:AddTerminalNavigation()
 
-	augroup TerminalNavigation
-	    autocmd!
-	    autocmd TermEnter * call s:AddTerminalNavigation()
-	augroup END
+        echom &filetype
+        if &filetype ==# ''
+      tnoremap <buffer> <silent> <c-h> <c-\><c-n>:TmuxNavigateLeft<cr>
+      tnoremap <buffer> <silent> <c-j> <c-\><c-n>:TmuxNavigateDown<cr>
+      tnoremap <buffer> <silent> <c-k> <c-\><c-n>:TmuxNavigateUp<cr>
+      tnoremap <buffer> <silent> <c-l> <c-\><c-n>:TmuxNavigateRight<cr>
+      tnoremap <Esc> <C-\><C-n>
+      tnoremap <C-v><Esc> <Esc>
+        endif
+
+    endfunction
+
+    augroup TerminalNavigation
+        autocmd!
+        autocmd TermEnter * call s:AddTerminalNavigation()
+    augroup END
+  endif
+
 endif
--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,39 +1,44 @@
-require('plugins')
+if vim.g.vscode then
 
--- Custom Plugins Configurations
-require('dante/nvim-autopairs') -- For completing pairs
+else
+  require('plugins')
 
-
-require('dante/cmp') -- This is a completion system to use with lsp and sources
-require('dante/nvim-lspconfig') -- Language System Protocol -- Universal
-require('dante/nvim-dap') -- Another debugger Debug Adapter Protocol -- Universal
--- require('dante/null-ls') -- Another debugger Debug Adapter Protocol -- Universal
-require('dante/snippets') -- Custom Snippets created by me
-require('dante/telescope') -- Like a file explorer or FZF
-require('dante/treesitter') -- ColorHighlight for a lot of languages
-require('dante/todo-comments') -- Highligh todo comments and show in a specific place
-require('dante/lualine') -- A fast but not fastest status line for neovim
-require('dante/comment') -- Comment plugin
-require('dante/indent-blankline') -- Show Indentation plugins
-require('stay-centered') -- Always make text centered
-require('dante/toggleterm') -- Terminal inside nvim
-require('dante/latex')
-require('dante/notify') -- Notify UI of plugins
-require('dante/emmet') -- Emmet for html and css
-require('dante/asynctasks')
-require('dante/git')
+  -- Custom Plugins Configurations
+  require('dante/nvim-autopairs') -- For completing pairs
 
 
-require('dante/others') -- Some custom plugins configurations
+  require('dante/cmp') -- This is a completion system to use with lsp and sources
+  require('dante/nvim-lspconfig') -- Language System Protocol -- Universal
+  require('dante/nvim-dap') -- Another debugger Debug Adapter Protocol -- Universal
+  -- require('dante/null-ls') -- Another debugger Debug Adapter Protocol -- Universal
+  require('dante/snippets') -- Custom Snippets created by me
+  require('dante/telescope') -- Like a file explorer or FZF
+  require('dante/treesitter') -- ColorHighlight for a lot of languages
+  require('dante/todo-comments') -- Highligh todo comments and show in a specific place
+  require('dante/lualine') -- A fast but not fastest status line for neovim
+  require('dante/comment') -- Comment plugin
+  require('dante/indent-blankline') -- Show Indentation plugins
+  require('stay-centered') -- Always make text centered
+  require('dante/toggleterm') -- Terminal inside nvim
+  require('dante/latex')
+  require('dante/notify') -- Notify UI of plugins
+  require('dante/emmet') -- Emmet for html and css
+  require('dante/asynctasks')
+  require('dante/git')
 
--- User Lua Configs
-require('utils')
-require('settings')
-require('keymappings')
-require('variables')
-require('autocmd')
-require('colors')
-require('dante/hydra')
 
--- Dante Plugins
-require('dante/processing')
+  require('dante/others') -- Some custom plugins configurations
+
+  -- User Lua Configs
+  require('utils')
+  require('settings')
+  require('keymappings')
+  require('variables')
+  require('autocmd')
+  require('colors')
+  require('dante/hydra')
+
+  -- Dante Plugins
+  require('dante/processing')
+
+end
