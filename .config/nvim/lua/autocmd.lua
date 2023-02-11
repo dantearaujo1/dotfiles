f8a4114ab77fac01aac3143dd2814c624d47af63
Dante Araújo <danteeng@hotmail.com>
2023-02-11 17:31:07 -0300

    Removing PackerCompile autocmd since im using lazy now

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -5,8 +5,6 @@ local util = require('utils')
 -- AutoCommands
 -- --------------------------------------------------------------------------
 cmd([[au BufEnter * set fo-=c fo-=r fo-=o]]) -- Dont auto comment new line
-
-cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]]) -- Command for recompile all plugins
 -- Remove whitespace on save
 cmd([[au BufWritePre * :%s/\s\+$//e]])
 
