feeeb0036381ef7ecf20e1ff85b718f075be6775
Dante Araújo <danteeng@hotmail.com>
2023-02-11 17:30:32 -0300

    Removing tabs icon shower in dashboard buffers and removing other things from dashboard buffers

--- .config/nvim/lua/dante/others.lua
+++ .config/nvim/lua/dante/others.lua
@@ -14,3 +14,6 @@ ccc.setup({
 
 local neoscroll = require('neoscroll')
 neoscroll.setup()
+
+vim.g.better_whitespace_filetypes_blacklist = { "dashboard" }
+vim.g.indent_blankline_filetype_exclude = { "dashboard" }
