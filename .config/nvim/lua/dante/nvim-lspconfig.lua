fdc8f4640e4063b88e3ff925428d39d46a38e207
Dante Araújo <danteeng@hotmail.com>
2023-02-22 17:24:18 -0300

    toggle signature key <M-x>

--- .config/nvim/lua/dante/nvim-lspconfig.lua
+++ .config/nvim/lua/dante/nvim-lspconfig.lua
@@ -6,24 +6,24 @@ capabilities.textDocument.foldingRange = {
   lineFoldingOnly = true
 }
 
-local options = { noremap=true, silent=true  }
+local options = { noremap = true, silent = true }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_next, options)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, options)
 vim.keymap.set('n', '<localleader>o', vim.diagnostic.open_float, options)
 vim.keymap.set('n', '<localleader>q', vim.diagnostic.setloclist, options)
 
- local signature_setup = {
+local signature_setup = {
   debug = false, -- set to true to enable debug logging
   log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
   -- default is  ~/.cache/nvim/lsp_signature.log
   verbose = false, -- show debug line number
 
   bind = true, -- This is mandatory, otherwise border config won't get registered.
-               -- If you want to hook lspsaga or other signature handler, pls set to false
+  -- If you want to hook lspsaga or other signature handler, pls set to false
   doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-                 -- set to 0 if you DO NOT want any API comments be shown
-                 -- This setting only take effect in insert mode, it does not affect signature help in normal
-                 -- mode, 10 by default
+  -- set to 0 if you DO NOT want any API comments be shown
+  -- This setting only take effect in insert mode, it does not affect signature help in normal
+  -- mode, 10 by default
 
   max_height = 12, -- max height of signature floating_window
   max_width = 80, -- max_width of signature floating_window
@@ -38,16 +38,16 @@ vim.keymap.set('n', '<localleader>q', vim.diagnostic.setloclist, options)
 
   floating_window_off_x = 1, -- adjust float windows x position.
   floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
-                              -- can be either number or function, see examples
+  -- can be either number or function, see examples
 
   close_timeout = 4000, -- close floating window after ms when laster parameter is entered
-  fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
+  fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
-  hint_prefix = "🐼 ",  -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+  hint_prefix = "🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
   hint_scheme = "String",
   hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
   handler_opts = {
-    border = "rounded"   -- double, rounded, single, shadow, none, or a table of borders
+    border = "rounded" -- double, rounded, single, shadow, none, or a table of borders
   },
 
   always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
@@ -62,7 +62,7 @@ vim.keymap.set('n', '<localleader>q', vim.diagnostic.setloclist, options)
   shadow_blend = 36, -- if you using shadow as border use this set the opacity
   shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+  toggle_key = '<M-x>', -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 
   select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
   move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
@@ -73,22 +73,22 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   require "lsp_signature".on_attach(signature_setup, bufnr)
   if client.server_capabilities.documentSymbolProvider then
-      navic.attach(client,bufnr)
+    navic.attach(client, bufnr)
   end
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>DS', require('telescope.builtin').lsp_document_symbols, bufopts)
-  vim.keymap.set('n', '<leader>WS', require('telescope.builtin').lsp_dynamic_workspace_symbols, bufopts)
   vim.keymap.set('n', '<localleader>gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<localleader>k', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<localleader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', '<localleader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>td', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>DS', require('telescope.builtin').lsp_document_symbols, bufopts)
+  vim.keymap.set('n', '<leader>WS', require('telescope.builtin').lsp_dynamic_workspace_symbols, bufopts)
   -- -- Workspace keymaps
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
@@ -100,82 +100,91 @@ local on_attach = function(client, bufnr)
 end
 
 local MASON_LSP_DEFAULT_SETTINGS = {
-    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
-    -- This setting has no relation with the `automatic_installation` setting.
-    ensure_installed = {'clangd','rust_analyzer','pyright','gopls', 'tsserver','sumneko_lua','clangd'},
+  -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
+  -- This setting has no relation with the `automatic_installation` setting.
+  ensure_installed = { 'clangd', 'rust_analyzer', 'pyright', 'gopls', 'tsserver', 'sumneko_lua', 'clangd' },
 
-    -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
-    -- This setting has no relation with the `ensure_installed` setting.
-    -- Can either be:
-    --   - false: Servers are not automatically installed.
-    --   - true: All servers set up via lspconfig are automatically installed.
-    --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
-    --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
-    automatic_installation = false,
+  -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+  -- This setting has no relation with the `ensure_installed` setting.
+  -- Can either be:
+  --   - false: Servers are not automatically installed.
+  --   - true: All servers set up via lspconfig are automatically installed.
+  --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+  --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+  automatic_installation = false,
 }
 require("mason").setup()
 require("mason-lspconfig").setup(MASON_LSP_DEFAULT_SETTINGS)
 require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-      require("lspconfig")[server_name].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-      }
-    end,
-    ["clangd"] = function ()
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end,
+  ["clangd"] = function()
+    require("lspconfig").clangd.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
       require("clangd_extensions").setup()
-    end,
-    ["sumneko_lua"] = function ()
-        require("lspconfig").sumneko_lua.setup {
-          on_attach = on_attach,
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              runtime = {
-                version = 'LuaJIT',
-                path = runtime_path,
-              },
-              diagnostics = {
-                globals = {'vim','use', 'require'},
-              },
-              workspace = {
-                maxPreload = 100000,
-                preloadFileSize = 100000,
-                library = {
-                  vim.api.nvim_get_runtime_file('',true),
-                  [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/love2d"] = true,
-                  -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/Jass"] = false,
-                  -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/OpenResty"] = false,
-                  -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/example"] = false,
-                  -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/Cocos4.0"] = false,
-                },
-                checkThirdParty = false,
-              },
-              telemetry = {
-                enable = false,
-              },
-            }
-          }
+    }
+  end,
+  ["jdtls"] = function()
+    --   require("lspconfig").jdtls.setup ({
+    --     on_attach = on_attach,
+    --     capabilities = capabilities,
+    --     root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mnvw'}, {upward = true})[0]),
+    -- })
+  end,
+  ["sumneko_lua"] = function()
+    require("lspconfig").sumneko_lua.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT',
+            path = runtime_path,
+          },
+          diagnostics = {
+            globals = { 'vim', 'use', 'require' },
+          },
+          workspace = {
+            maxPreload = 100000,
+            preloadFileSize = 100000,
+            library = {
+              vim.api.nvim_get_runtime_file('', true),
+              [vim.fn.expand "~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/love2d"] = true,
+              -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/Jass"] = false,
+              -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/OpenResty"] = false,
+              -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/example"] = false,
+              -- [vim.fn.expand"~/nvim/nvim-data/lsp_servers/sumneko_lua/extension/server/meta/3rd/Cocos4.0"] = false,
+            },
+            checkThirdParty = false,
+          },
+          telemetry = {
+            enable = false,
+          },
         }
-    end
+      }
+    }
+  end
 }
 
 
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- virtual_text = {
-    --   prefix = ">>",
-    --   spacing = 2
-    -- },
-    virtual_text = false,
-    checkCurrentLine = true,
-    signs = true,
-    update_in_insert = false,
- }
+  -- virtual_text = {
+  --   prefix = ">>",
+  --   spacing = 2
+  -- },
+  virtual_text = false,
+  checkCurrentLine = true,
+  signs = true,
+  update_in_insert = false,
+}
 )
-
-
