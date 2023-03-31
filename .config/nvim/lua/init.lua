b207e89b86b4484f338c77d3a91afd4e91c73700
Dante Araújo <danteeng@hotmail.com>
2023-03-31 13:46:26 -0300

    Correcting some settings

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -25,24 +25,24 @@ else
   require('dante/nvim-autopairs') -- For completing pairs
 
 
-  require('dante/cmp') -- This is a completion system to use with lsp and sources
-  require('dante/nvim-lspconfig') -- Language System Protocol -- Universal
-  require('dante/nvim-dap') -- Another debugger Debug Adapter Protocol -- Universal
-  -- require('dante/null-ls') -- Another debugger Debug Adapter Protocol -- Universal
-  require('dante/snippets') -- Custom Snippets created by me
-  require('dante/telescope') -- Like a file explorer or FZF
-  require('dante/treesitter') -- ColorHighlight for a lot of languages
-  require('dante/todo-comments') -- Highligh todo comments and show in a specific place
-  require('dante/lualine') -- A fast but not fastest status line for neovim
-  require('dante/comment') -- Comment plugin
-  require('dante/indent-blankline') -- Show Indentation plugins
-  require('stay-centered') -- Always make text centered
-  require('dante/toggleterm') -- Terminal inside nvim
-  require('dante/latex')
-  require('dante/notify') -- Notify UI of plugins
-  require('dante/emmet') -- Emmet for html and css
-  require('dante/asynctasks')
-  require('dante/git')
+   require('dante/cmp') -- This is a completion system to use with lsp and sources
+   require('dante/nvim-lspconfig') -- Language System Protocol -- Universal
+   require('dante/nvim-dap') -- Another debugger Debug Adapter Protocol -- Universal
+   -- require('dante/null-ls') -- Another debugger Debug Adapter Protocol -- Universal
+   require('dante/snippets') -- Custom Snippets created by me
+   require('dante/telescope') -- Like a file explorer or FZF
+   require('dante/treesitter') -- ColorHighlight for a lot of languages
+   require('dante/todo-comments') -- Highligh todo comments and show in a specific place
+   require('dante/lualine') -- A fast but not fastest status line for neovim
+   require('dante/comment') -- Comment plugin
+   require('dante/indent-blankline') -- Show Indentation plugins
+   require('stay-centered') -- Always make text centered
+   require('dante/toggleterm') -- Terminal inside nvim
+   require('dante/latex')
+   require('dante/notify') -- Notify UI of plugins
+   require('dante/emmet') -- Emmet for html and css
+   require('dante/asynctasks')
+   require('dante/git')
 
   -- require('dante/firenvim') -- Browser Configurations for Firenvim
 
