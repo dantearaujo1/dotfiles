9dab621163674028dd16d3b4bb17b83baac19497
Dante Araújo <danteeng@hotmail.com>
2023-03-31 16:14:43 -0300

    Adjusting wezterm cwd for home

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local wsl_domains = wezterm.default_wsl_domains()
 
 -- This table will hold the configuration.
 local config = {}
@@ -15,7 +16,20 @@ end
 -- For example, changing the color scheme:
 config.color_scheme = 'Palenight (Gogh)'
 config.font = wezterm.font 'JetBrains Mono'
-config.default_prog = {'wsl.exe', '-d', 'Ubuntu'}
+config.default_cwd = os.getenv("HOME")
+-- config.default_wsl_domains = wsl_domains
+
+-- if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+--     config.default_prog = {
+--         "nu"
+--     }
+-- end
+--
+-- if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+--     config.default_prog = {
+--         "/home/mib/.rustup/bin/nu"
+--   }
+-- end
 
 -- and finally, return the configuration to wezterm
 return config
