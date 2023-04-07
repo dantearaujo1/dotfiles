5e3a0df6cbe648bc6e10d5924c2ff3612f24979b
Dante Araújo <danteeng@hotmail.com>
2023-04-07 16:55:00 -0300

    Change theme to gruvbox-baby

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -59,9 +59,9 @@ w.signcolumn = 'yes'				-- Set a Column before numbers
 
 -- cmd('colorscheme aurora')                    -- Setting the last used theme
 -- vim.opt.background = 'dark'
+cmd('colorscheme gruvbox-baby')                    -- Setting the theme
 vim.g.gruvbox_baby_background_color = 'dark'
 vim.g.gruvbox_baby_telescope_theme = 1
 vim.g.gruvbox_baby_transparent_mode = 1
-cmd('colorscheme gruvbox')                    -- Setting the theme
 -- --------------------------------------------------------------------------
 
