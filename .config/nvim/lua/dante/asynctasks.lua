75570a424dc03401b38400cf0936f0f891f2e6d1
Dante de Araujo <danteeng@hotmail.com>
2023-04-30 21:10:46 -0300

    Adjusting asynctasks so it can uses correct term pos inside windows and linux with tmux

--- .config/nvim/lua/dante/asynctasks.lua
+++ .config/nvim/lua/dante/asynctasks.lua
@@ -2,4 +2,8 @@ local g = vim.g
 
 g.asyncrun_open = 6
 g.asyncrun_rootmarks = { ".git",".svn",'.root','.project','.hg','src' }
-g.asyncrun_term_pos = "tmux"
+if vim.fn.exists('$TMUX') then
+  g.asyncrun_term_pos = "tmux"
+else
+  g.asyncrun_term_pos = "bottom"
+end
