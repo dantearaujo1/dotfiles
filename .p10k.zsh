096438b685e3268d6d878eb953d6ef19a517f229
Dante de Araujo <danteeng@hotmail.com>
2023-05-19 21:10:48 -0300

    Removing username@hostname from prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -31,8 +31,8 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    # os_icon                 # os identifier
-    context                 # username@hostname
+    os_icon                 # os identifier
+    # context                 # username@hostname
     dir                     # current directory
     vcs                     # git status
     prompt_char           # prompt symbol
