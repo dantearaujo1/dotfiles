1c45d9c6782662b3fea4808e4f5e1f51bd6494be
Dante de Araujo <danteeng@hotmail.com>
2024-03-29 09:35:25 -0300

    qutebrowser config changes

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,26 +1,22 @@
-# import dracula.draw
-# import catppuccin
 config.load_autoconfig()
-# Setting dark mode
-# config.set("colors.webpage.darkmode.enabled", True)
-#
-# dracula.draw.blood(c,{
-#     'spacing':{
-#         'vertical':6,
-#         'horizontal':8
-#     }
-# })
-#
-#
-# catppuccin.setup(c,'mocha',True)
 
-# config.bind(";", 'open')
-config.bind(",u", 'spawn --userscript ~/.config/qutebrowser/userscripts/qute_1pass login')
+config.bind("<Ctrl+Alt+1>", 'tab-focus 1')
+config.bind("<Ctrl+Alt+2>", 'tab-focus 2')
+config.bind("<Ctrl+Alt+3>", 'tab-focus 3')
+config.bind("<Ctrl+Alt+4>", 'tab-focus 4')
+config.bind("<Ctrl+Alt+5>", 'tab-focus 5')
+config.bind("<Ctrl+Alt+6>", 'tab-focus 6')
+config.bind("<Ctrl+Alt+7>", 'tab-focus 7')
+config.bind("<Ctrl+Alt+8>", 'tab-focus 8')
+config.bind("<Ctrl+Alt+9>", 'tab-focus 9')
+config.bind("<Ctrl+Alt+0>", 'tab-focus 10')
+config.bind(",u", 'spawn --userscript qute-1pass')
 config.bind(',b','hint links spawn mpv {hint-url}')
 config.bind(',B','spawn mpv {url}')
 config.bind(',g','tab-give')
 config.bind("<Ctrl-=>", 'zoom-in')
 config.bind("<Ctrl-->", 'zoom-out')
+config.bind("<Ctrl+r>", 'config-source')
 config.bind(",p", 'cmd-set-text :open -p ')
 config.bind(',ko', 'jseval (function () { '+
 '  var i, elements = document.querySelectorAll("body *");'+
@@ -41,9 +37,9 @@ c.url.searchengines['w'] = 'https://en.wikipedia.org/w/index.php?search={}'
 c.url.searchengines['gh'] = 'https://github.com/search?q={}&type=Code'
 c.url.searchengines['ap'] = 'https://www.archlinux.org/packages/?sort=&q={}'
 c.url.searchengines['ab'] = 'https://bugs.archlinux.org/?project=5&string={}'
+c.url.searchengines['gp'] = 'http://127.0.0.1:{}'
 
 c.aliases['ge'] = 'open -t https://wiki.archlinux.org/index.php/Forum_Etiquette'
-c.aliases['gp'] = 'open -t http://127.0.0.1:4000'
 c.aliases['gu'] = 'open -t https://github.com/dantearaujo1'
 
 # if c.colors.webpage.darkmode.enabled:
@@ -52,7 +48,8 @@ c.aliases['gu'] = 'open -t https://github.com/dantearaujo1'
 c.colors.tabs.selected.even.bg = "#AA00aa"
 c.colors.tabs.selected.even.fg = "white"
 c.tabs.background = True
-# c.content.pdfjs = True
+c.content.pdfjs = True
+c.content.fullscreen.window = True
 
 c.hints.selectors["code"] = [
     # Selects all code tags whose direct parent is not a pre tag
@@ -61,6 +58,7 @@ c.hints.selectors["code"] = [
 ]
 
 # c.content.javascript.clipboard = "access"
+c.content.local_content_can_access_remote_urls = True
 with config.pattern('*://github.com/') as p:
     p.content.javascript.clipboard = "access-paste"
 with config.pattern('*://youtube.com/') as p:
@@ -85,3 +83,4 @@ config.bind(';', 'cmd-set-text :')
 config.bind('<Ctrl+T>', 'spawn --userscript translate')
 config.bind('<Ctrl+Shift+T>', 'spawn --userscript translate --text')
 config.source('themes/qute-city-lights/city-lights-theme.py')
+config.bind("<Ctrl+w>", 'cmd-set-text :open -w ')
